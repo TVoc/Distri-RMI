a2b25a9bfd8227cf4ba7cb49a129cdb46579dfef
TVoc <thomasvochten@hotmail.com>
2014-10-28 10:59:47 +0100

    stuff

--- CarRentalCompany/src/sessions/IReservationSession.java
+++ CarRentalCompany/src/sessions/IReservationSession.java
@@ -1,5 +1,22 @@
 package sessions;
 
-public interface IReservationSession {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Date;
 
+import rental.Quote;
+import rental.ReservationException;
+
+public interface IReservationSession extends Remote, Session {
+
+	public void createQuote(Date start, Date end, String carType, String companyName) throws RemoteException, ReservationException;
+	
+	public Collection<Quote> getCurrentQuotes() throws RemoteException;
+	
+	public void confirmQuotes() throws RemoteException, ReservationException;
+	
+	public void checkAvailableCarTypes(Date start, Date end) throws RemoteException;
+	
+	
 }
