73392982134db9c96487c3248d68077eef0a7e9b
BeardedPlatypus <mwtegelaers@gmail.com>
2014-10-28 10:31:36 +0100

    That agency though.

---
+++ CarRentalCompany/src/agency/RentalAgency.java
@@ -0,0 +1,70 @@
+package agency;
+
+import naming.INamingService;
+import rental.ICarRentalCompany;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Mediator Server that serves as access point for both the INamingService of
+ * the system, as well as the SessionManager.
+ */
+public class RentalAgency implements Remote {
+    //------------------------------------------------------------------------
+    // Constructor
+    //------------------------------------------------------------------------
+    public RentalAgency() {
+
+    }
+
+    //------------------------------------------------------------------------
+    // Naming service
+    //------------------------------------------------------------------------
+    /**
+     * Register a new ICarRentalCompany in the INamingService of this
+     * RentalAgency
+     *
+     * @param newCompany
+     *      The new IRentalCompany to be registered within this RentalAgency.
+     *
+     * @throws RemoteException
+     *     | ICarRentalCompany == null
+     *     | ICarRentalCompany.getName() == null
+     */
+    public void register(ICarRentalCompany newCompany) throws RemoteException {
+        this.getNamingService().register(newCompany);
+    }
+
+    /**
+     * Unregister the ICarRentalCompany with the specified name from
+     * this RentalAgency INamingService.
+     *
+     * @param name
+     *      The name of the ICarRentalCompany to be removed.
+     *
+     * @throws RemoteException
+     *      | name == null
+     */
+    public void unregister(String name) throws RemoteException {
+        this.getNamingService().unregister(name);
+    }
+
+    /**
+     * Get the INamingService of this RentalAgency.
+     *
+     * @return The INamingService of this RentalAgency.
+     */
+    private INamingService getNamingService() {
+        return this.namingService;
+    }
+
+    /** The INamingService of this RentalAgency. */
+    private INamingService namingService;
+
+    //------------------------------------------------------------------------
+    // SessionManager
+    //---------------------------------------------    ---------------------------
+    /** The SessionManager of this RentalAgency */
+    private final SessionManager sessionManager;
+}
