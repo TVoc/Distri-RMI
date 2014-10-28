c0a105c628a0838cc5581f41442c4009c228a7f2
BeardedPlatypus <mwtegelaers@gmail.com>
2014-10-28 12:34:28 +0100

    je moeder

---
+++ CarRentalCompany/src/sessions/ManagerSession.java
@@ -0,0 +1,189 @@
+package sessions;
+
+import naming.ICompanyIterator;
+import rental.CarType;
+import rental.ICarRentalCompany;
+import utilities.MapHighestFinder;
+import utilities.MapMerger;
+
+import java.rmi.RemoteException;
+import java.util.*;
+
+public class ManagerSession implements IManagerSession {
+    public ManagerSession(SessionManager sessionManager, RentalAgency rentalAgency) {
+        this.sessionManager = sessionManager;
+        this.rentalAgency = rentalAgency;
+    }
+
+    @Override
+    public void register(ICarRentalCompany newComp) {
+        this.getRentalAgency().register(newComp);
+    }
+
+    @Override
+    public void unregister(String name) {
+        this.getRentalAgency().unregister(name);
+    }
+
+    @Override
+    public List<String> getRegisteredCompanies() throws RemoteException {
+        ICompanyIterator iter = this.getSessionManager().getCompanyIterator();
+
+        List<String> res = new ArrayList<String>();
+        ICarRentalCompany temp = null;
+        while (iter.hasNext()) {
+            temp = iter.next();
+            res.add(temp.getName());
+        }
+        return res;
+    }
+
+    @Override
+    public List<CarType> getAvailableCarTypesIn(String companyName) {
+        return this.getSessionManager().lookupCompany(companyName).getAvailableCarTypes();
+    }
+
+    @Override
+    public int getNumberOfReservationsBy(String clientName) throws RemoteException {
+        ICompanyIterator iter = this.getSessionManager().getCompanyIterator();
+        long timestamp = System.currentTimeMillis();
+
+        int res = 0;
+        ICarRentalCompany temp = null;
+        while (iter.hasNext()) {
+            temp = iter.next();
+            res += temp.getNumReservationsPerCustomer(timestamp).get(clientName);
+        }
+        return res;
+    }
+
+    /*
+    ──────────────────────────────────
+    ───────────────▀█▄█▀──────────────
+    ────────▄█████▄─███───────────────
+    ──────▄████████████▄██████▄───────
+    ──────██████████████████████──────
+    ──────██████████████████████──────
+    ──────██████████████████████──────
+    ──────█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█──────
+    ──────██████████████████████──────
+    ────▄█▀▀──────────────────▀▀█▄────
+    ──▄█▀────────────────────────▀█▄──
+    ─█▀────▄▀▀▀▀▄────────▄▀▀▀▀▄────▀█─
+    ─█────█──▄▄──█─▄▀▀▄─█──▄▄──█────█─
+    ─█────█─████─██░░░░██─████─█────█─
+    ██──▄▄███████▀░░░░░░▀███████▄▄──██
+    █──▀█░░░░░░░░░░▄▄▄▄░░░░░░░░░░█▀──█
+    █───▀█▀▄▄▄▄▄▄▀▀░░░░▀▀▄▄▄▄▄▄▀█▀───█
+    █────▀▄░░░░░░░░▄▄▄▄░░░░░░░░▄▀────█
+    ██─────▀▀▀▀▀▀▀▀────▀▀▀▀▀▀▀▀─────██
+    ─█──────────────────────────────█─
+    ─█▄────────────────────────────▄█─
+    ──█▄──────────────────────────▄█──
+    ───█▄────────────────────────▄█───
+    ──────────────────────────────────
+     */
+    @Override
+    public Set<String> getBestClient() throws RemoteException {
+        ICompanyIterator iter = this.getSessionManager().getCompanyIterator();
+        long timestamp = System.currentTimeMillis();
+
+        Map<String, Integer> res_raw = new HashMap<String, Integer>();
+        MapMerger<String> leMapMerger = new MapMerger<String>();
+
+        ICarRentalCompany temp = null;
+
+
+        while (iter.hasNext()) {
+            temp = iter.next();
+            leMapMerger.merge(res_raw, temp.getNumReservationsPerCustomer(timestamp));
+        }
+
+        Set<String> res = new HashSet<String>();
+
+        MapHighestFinder<String> highestFinder = new MapHighestFinder<String>();
+
+        String key = highestFinder.findHighestIn(res_raw);
+        Integer max = res_raw.get(key);
+
+        res.add(key);
+        res_raw.remove(key);
+
+        while(res_raw.values().contains(max)) {
+            key = highestFinder.findHighestIn(res_raw);
+            res.add(key);
+            res_raw.remove(key);
+        }
+
+        return res;
+    }
+
+    @Override
+    public int getNumberOfReservationsForCarType(String carRentalCompanyName, String carType) {
+        return this.getSessionManager().lookupCompany(carRentalCompanyName).getNumberOfReservationsFor(carType, System.currentTimeMillis());
+    }
+
+    @Override
+    public CarType getMostPopularCarTypeIn(String carRentalCompanyName) {
+        return this.getSessionManager().lookupCompany(carRentalCompanyName).getMostPopularCarType(System.currentTimeMillis());
+    }
+
+    @Override
+    public SessionManager getSessionManager() {
+        return this.sessionManager;
+    }
+
+    private final SessionManager sessionManager;
+
+    public RentalAgency getRentalAgency() {
+        return this.rentalAgency;
+    }
+
+    private final RentalAgency rentalAgency;
+
+    //--------------------------------------------------------------------
+    // UNSUPPORTED STUFF
+    /*
+            ─────────▄██████▀▀▀▀▀▀▄
+            ─────▄█████████▄───────▀▀▄▄
+            ──▄█████████████───────────▀▀▄
+            ▄██████████████─▄▀───▀▄─▀▄▄▄──▀▄
+            ███████████████──▄▀─▀▄▄▄▄▄▄────█
+            █████████████████▀█──▄█▄▄▄──────█
+            ███████████──█▀█──▀▄─█─█─█───────█
+            ████████████████───▀█─▀██▄▄──────█
+            █████████████████──▄─▀█▄─────▄───█
+            █████████████████▀███▀▀─▀▄────█──█
+            ████████████████──────────█──▄▀──█
+            ████████████████▄▀▀▀▀▀▀▄──█──────█
+            ████████████████▀▀▀▀▀▀▀▄──█──────█
+            ▀████████████████▀▀▀▀▀▀──────────█
+            ──███████████████▀▀─────█──────▄▀
+            ──▀█████████████────────█────▄▀
+            ────▀████████████▄───▄▄█▀─▄█▀
+            ──────▀████████████▀▀▀──▄███
+            ──────████████████████████─█
+            ─────████████████████████──█
+            ────████████████████████───█
+            ────██████████████████─────█
+            ────██████████████████─────█
+            ────██████████████████─────█
+            ────██████████████████─────█
+            ────██████████████████▄▄▄▄▄█
+    */
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+    @Override
+    public long getTimeStamp() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException();
+    }
+
+}
